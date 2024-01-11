57e73cf8475084b0389b5f0f6fcf6d815a735eff
JamieBurridge <xross_dev@proton.me>
2024-01-11 22:22:07 +0000

    fix image not downloading

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,42 +2,40 @@ import { toPng } from "html-to-image";
 import { saveAs } from "file-saver";
 
 window.onload = () => {
-    const buttonEl = document.getElementsByTagName("button")[0];
-    const textareaEl = document.getElementsByTagName("textarea")[0];
-    const outputSection = document.getElementsByTagName("section")[0];
-    const outputTextEl = document.getElementById("output_meme_text");
+  const buttonEl = document.getElementsByTagName("button")[0];
+  const textareaEl = document.getElementsByTagName("textarea")[0];
+  const outputSection = document.getElementsByTagName("section")[0];
+  const outputTextEl = document.getElementById("output_meme_text");
 
-    // Click button and create meme
-    buttonEl.addEventListener("click", () => {
-        const memeText = makeAlternatingCase(textareaEl.value);
+  // Click button and create meme
+  buttonEl.addEventListener("click", async () => {
+    const memeText = makeAlternatingCase(textareaEl.value);
 
-        outputSection.style.display = "block";
-        outputTextEl.textContent = memeText;
+    outputSection.style.display = "block";
+    outputTextEl.textContent = memeText;
 
-        // Convert HTML to image and download
-        saveAs(convertElementToImage(outputSection), "mocking-spongebob-meme.png");
-    });
-    
-    
-    // Make meme text
-    function makeAlternatingCase(text) {
-        let alternateCaseText = "";
+    // Convert HTML to image and download
+    const imageToDownload = await convertElementToImage(outputSection);
+    saveAs(imageToDownload, "mocking-spongebob-meme.png");
+  });
 
-        for (let i = 0; i < text.length; i++) {
-            if (i % 2 === 0) {
-                alternateCaseText += text[i].toLowerCase();
-            } else {
-                alternateCaseText += text[i].toUpperCase();
-            }
-        }
+  // Make meme text
+  function makeAlternatingCase(text) {
+    let alternateCaseText = "";
 
-        return alternateCaseText;
+    for (let i = 0; i < text.length; i++) {
+      if (i % 2 === 0) {
+        alternateCaseText += text[i].toLowerCase();
+      } else {
+        alternateCaseText += text[i].toUpperCase();
+      }
     }
 
-    // Convert element to image
-    function convertElementToImage(element) {
-        toPng(element).then(function (dataUrl) {
-            return dataUrl;
-        });
-    }
-}
+    return alternateCaseText;
+  }
+
+  // Convert element to image
+  async function convertElementToImage(element) {
+    return await toPng(element);
+  }
+};
